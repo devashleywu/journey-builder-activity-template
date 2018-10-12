4bee92b3a78a5c503b4f1b54c20694df22f15cd0
Ashley Wu <>
2018-10-12 11:40:14 +1100

    add stop route

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
 app.use(bodyParser.raw({type: 'application/jwt'}));
 //app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,6 +39,7 @@ app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
+app.post('/journeybuilder/stop/', activity.stop );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
