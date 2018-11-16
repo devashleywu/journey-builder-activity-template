0c29d7ba6edb2396a30cf5db856032a445a35d7b
Ashley Wu <>
2018-11-16 15:07:04 +1100

    remove validate function for now

--- routes/activity.js
+++ routes/activity.js
@@ -230,14 +230,14 @@ exports.validate = function (req, res) {
             return res.status(401).end();
         }
 
-        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
-            var messageTag = decodedArgs.messageTag;
-            if (messageTag.length > 0) {
-                res.status(200).send('Validate');
-            }
-        }
+        // if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+        //     // decoded in arguments
+        //     var decodedArgs = decoded.inArguments[0];
+        //     var messageTag = decodedArgs.messageTag;
+        //     if (messageTag.length > 0) {
+        //         res.status(200).send('Validate');
+        //     }
+        // }
     });
 
 
